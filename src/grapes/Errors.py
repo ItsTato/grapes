c038720c24043b0099242d47694cfa5763bc377a
ItsTato <itstato.official@gmail.com>
2024-03-26 02:41:58 -0600

    Add in custom errors and types for columns

---
+++ src/grapes/Errors.py
@@ -0,0 +1,9 @@
+class TableAlreadyExists(Exception):
+    def __init__(self, message:str) -> None:
+        super().__init__(message)
+        self.message:str = message
+
+class TableNameIsBlankOrInvalid(Exception):
+    def __init__(self, message:str) -> None:
+        super().__init__(message)
+        self.message:str = message
---
+++ src/grapes/Types.py
@@ -0,0 +1,10 @@
+class Type:
+    def __init__(self) -> None:
+        return
+
+STRING:Type = Type()
+INTEGER:Type = Type()
+FLOAT:Type = Type()
+DICTIONARY:Type = Type()
+BOOLEAN:Type = Type()
+LIST:Type = Type()
