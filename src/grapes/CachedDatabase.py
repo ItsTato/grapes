455723d69157d107be27ffb700585e07c0d0f4e6
ItsTato <itstato.official@gmail.com>
2024-03-26 20:04:44 -0600

    Remove comment UwU

--- src/grapes/CachedDatabase.py
+++ src/grapes/CachedDatabase.py
@@ -1,11 +1,4 @@
-import os
 from .Database import Database
-
-# Tato's Notes-To-Self #
-
-# Use shutil for backup operations
-# (it can handle big copies of folders easily)
-# (same with compression)
 
 class CachedDatabase(Database):
 	def __init__(self,data_directory:str="/data") -> None:
