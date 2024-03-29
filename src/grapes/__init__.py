8082c3a29c463e2b2e652d3df4660f1960acdc5b
ItsTato <itstato.official@gmail.com>
2024-03-29 00:40:11 -0600

    Invalid import

--- src/grapes/__init__.py
+++ src/grapes/__init__.py
@@ -1,6 +1,5 @@
 from .GrapesDatabase import GrapesDatabase
 from .InMemoryGrapesDatabase import InMemoryGrapesDatabase
-from .CachedGrapesDatabase import CachedGrapesDatabase
 from .Table import Table
 from .Column import Column
 from . import Types
