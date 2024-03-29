950922a2811f897ea3680e3e1dfdb2dfda5cfafb
ItsTato <itstato.official@gmail.com>
2024-03-28 22:32:33 -0600

    Leave that for later.

--- src/grapes/CachedGrapesDatabase.py
+++
@@ -1,7 +0,0 @@
-from .GrapesDatabase import GrapesDatabase
-
-class CachedGrapesDatabase(GrapesDatabase):
-	def __init__(self,data_directory:str="/data") -> None:
-		super().__init__(data_directory=data_directory)
-		self.__cache:dict = {}
-		return
