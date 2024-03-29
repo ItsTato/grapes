154f409d38f7bc46febacd220a0dc5c436fc5971
ItsTato <itstato.official@gmail.com>
2024-03-29 03:06:05 -0600

    TypeError. Oh god.

--- src/grapes/Errors/InsertError.py
+++ src/grapes/Errors/InsertError.py
@@ -18,3 +18,10 @@ class EmptyRequest(Exception):
 	@property
 	def message(self) -> str:
 		return self.__message
+
+class TypeError(Exception):
+	def __init__(self, message:str) -> None:
+		self.__message:str = message
+	@property
+	def message(self) -> str:
+		return self.__message
