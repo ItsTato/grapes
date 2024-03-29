af524a12bf5531cc00ff1d313ab3fa97621b834e
ItsTato <itstato.official@gmail.com>
2024-03-29 03:40:34 -0600

    Update test.py (All tests worked! =3)

--- src/test.py
+++ src/test.py
@@ -12,8 +12,17 @@ if not db.has_table("Users"):
 		]
 	))
 
-db.insert_into("Users",(37,"Howdy",1))
+db.insert_into("Users",(1,"Tato",400))
+db.insert_into("Users",(2,"pelele",200))
+db.insert_into("Users",(3,"ea_ea",-1200))
+
+print(db.get_where("Users","ID",1))
+print(db.get_where("Users","Name","pelele"))
+print(db.get_where("Users","Robux",-1200))
+
 print(db.get_all("Users"))
+
+exit()
 
 from grapes import InMemoryGrapesDatabase
 db2:InMemoryGrapesDatabase = InMemoryGrapesDatabase("/data2")
