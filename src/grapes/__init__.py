e428e2982cc129edf20edd893e95d93bd99e73ea
ItsTato <78940659+ItsTato@users.noreply.github.com>
2024-04-06 19:58:29 -0600

    know tf youre dealing with. - just in case

--- src/grapes/__init__.py
+++ src/grapes/__init__.py
@@ -3,3 +3,4 @@ from .InMemoryGrapesDatabase import InMemoryGrapesDatabase
 from .Table import Table
 from .Column import Column
 from . import Types
+from .__meta import GRAPES_VERSION, PYTHON_VERSION
