357be6a45499ec79a5a06692f840df49eb8c4d06
ItsTato <78940659+ItsTato@users.noreply.github.com>
2024-05-19 10:33:54 -0600

    Renames

--- src/grapes/__init__.py
+++ src/grapes/__init__.py
@@ -2,4 +2,4 @@ from .GrapesDatabase import GrapesDatabase
 from .InMemoryGrapesDatabase import InMemoryGrapesDatabase
 from .Table import Table
 from .Column import Column
-from .__meta__ import GRAPES_VERSION, PYTHON_VERSION
+from .__meta__ import GRAPES_VERSION as __version__, PYTHON_VERSION as __py_version__
